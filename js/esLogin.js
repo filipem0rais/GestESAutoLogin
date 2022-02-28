eb4ede252b0c40bb70c75d8670c3ffc3331c169d
Dias Morais Filipe <diasmorais.filipe@gmail.com>
2022-02-28 10:38:42 +0100

    Stop when login error

--- js/esLogin.js
+++ js/esLogin.js
@@ -1,10 +1,17 @@
-if(document.getElementById("username") != null){
-    chrome.storage.sync.get("user", function(result) {
-        if(result.user !== undefined){
-            document.getElementById("username").value = result.user.email
-            document.getElementById("password").value = result.user.password
 
-            document.querySelector('button').click()
-        }
-    })
+let elements = document.getElementsByClassName('message error');
+let myCollection = Array.from(elements)
+
+if(myCollection.length === 0){
+    if(document.getElementById("username") != null){
+        chrome.storage.sync.get("user", function(result) {
+            if(result.user !== undefined){
+                document.getElementById("username").value = result.user.email
+                document.getElementById("password").value = result.user.password
+
+                document.querySelector('button').click()
+            }
+        })
+    }
 }
+
